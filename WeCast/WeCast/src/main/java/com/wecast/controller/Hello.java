f5bbe851f59d3315b5fcec552f42d3844a11fbe0
RavindraMorghade <ravimorghade@gmail.com>
2021-10-06 12:38:12 +0530

    Hello File changed

--- WeCast/WeCast/src/main/java/com/wecast/controller/Hello.java
+++ WeCast/WeCast/src/main/java/com/wecast/controller/Hello.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 	
-	String str="Hello Neha..";
+	String str="Hello Neha...";
 	
 	@GetMapping("/hello")
 	public String showData(){
